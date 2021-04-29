54df3a7dac864f12a88c725ddc45c337d2d1dc68
TimmyLuong11 <Timothy.luong@ou.edu>
2021-04-28 22:12:25 -0500

    Initial commit

---
+++ src/hw3.java
@@ -0,0 +1,15 @@
+
+public class hw3 
+{
+	public static void main(String[] args)
+	{
+		int product = 5;
+	     for (int count = 0; product > 0; ++count)
+	     {
+	       product = product * product;
+	       System.out.println(product + " " + count);
+
+	     }
+	 }
+		
+}
